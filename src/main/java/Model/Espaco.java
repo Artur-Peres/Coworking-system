edcbc870989a48d88622e8fc76c08be4156d98df
Bianca Mirelle <biancamirellefg49@gmail.com>
2025-12-04 19:09:19 -0300

    Update Espaco.java

--- src/main/java/Model/Espaco.java
+++ src/main/java/Model/Espaco.java
@@ -18,24 +18,47 @@ public abstract class Espaco {
     }
 
     // Getters e Setters
-    public int getId() { return id; }
-    public void setId(int id) { this.id = id; }
+    public int getId() { 
+        return id; 
+    }
+    public void setId(int id) { 
+        this.id = id;
+    }
 
-    public String getNome() { return nome; }
-    public void setNome(String nome) { this.nome = nome; }
+    public String getNome() { 
+        return nome;
+    }
+    public void setNome(String nome) { 
+        this.nome = nome; 
+    }
 
-    public int getCapacidade() { return capacidade; }
-    public void setCapacidade(int capacidade) { this.capacidade = capacidade; }
+    public int getCapacidade() {
+        return capacidade;
+    }
+    public void setCapacidade(int capacidade) {
+        this.capacidade = capacidade;
+    }
 
-    public boolean isDisponivel() { return disponivel; }
-    public void setDisponivel(boolean disponivel) { this.disponivel = disponivel; }
+    public boolean isDisponivel() {
+        return disponivel; 
+    }
+    public void setDisponivel(boolean disponivel) { 
+        this.disponivel = disponivel;
+    }
 
-    public double getPrecoPorHora() { return precoPorHora; }
-    public void setPrecoPorHora(double precoPorHora) { this.precoPorHora = precoPorHora; }
+    public double getPrecoPorHora() { 
+        return precoPorHora;
+    }
+    public void setPrecoPorHora(double precoPorHora) {
+        this.precoPorHora = precoPorHora;
+    }
 
-    public String getTipo() {return tipo;}
+    public String getTipo() {
+        return tipo;
+    }
 
     // Método abstrato
     public abstract double calcularCustoReserva(int horas);
+
 
 }
